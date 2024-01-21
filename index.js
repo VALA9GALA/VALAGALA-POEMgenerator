1dbdb034eb873c2aae2a93d5abeef4cd84d2395e
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-01-20 23:44:57 -0500

    basic form handling w JS
    and added Typewriter plug-in

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: `Such verses for my Body let us write, (for we are one)`,
+    delay: 80,
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let botPoetForm = document.querySelector("#bot-poet-form");
+botPoetForm.addEventListener("submit", generatePoem);
